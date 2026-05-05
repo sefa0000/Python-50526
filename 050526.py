0688eb84ce48894f1a28ee7ca69f4cd280260e27
sefa0000 <sefakasap05@gmail.com>
2026-05-05 20:14:46 +0300

    Update 050526.py

--- 050526.py
+++ 050526.py
@@ -1,8 +1,12 @@
-def round_up(x):
-    return round(x + 0.5)
+def round_custom(x):
+    tam_kisim = int(x)
+    ondalik = x - tam_kisim
 
-def round_down(x):
-    return round(x - 0.5)
+    if ondalik >= 0.5:
+        return tam_kisim + 1
+    else:
+        return tam_kisim
 
-print("Up:", round_up(3.2))
-print("Down:", round_down(3.8))
+
+sayi = float(input("Bir sayi gir: "))
+print("Sonuc:", round_custom(sayi))
